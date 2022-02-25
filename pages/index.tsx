8d196a5e617d8bfa953a78e8b85e152f1fdc8a1f
Osman <72527925+rosman83@users.noreply.github.com>
2022-02-24 22:58:21 -0600

    Adding some blocking

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,84 +1,100 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import { useState } from 'react';
+// import Image from 'next/image'
 
 const Home: NextPage = () => {
+  let tag = "Invasion#2063"
+  let email = "hi@invader.codes"
+
+  const [disc, setDisc] = useState(tag);
+  const [ema, setEma] = useState(email);
+
+  const delay = (ms: any) => new Promise(res => setTimeout(res, ms));
+
+  async function saveDiscord() {
+    navigator.clipboard.writeText(tag);
+    setDisc("Copied!")
+    await delay(5000);
+    setDisc(tag);
+  }
+
+  async function saveEmail() {
+    navigator.clipboard.writeText(email);
+    setEma("Copied!");
+    await delay(5000);
+    setEma(email);
+  }
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="core flex min-h-screen flex-col items-center justify-center">
       <Head>
-        <title>Create Next App</title>
+        <title>Invader - Development</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main className="flex w-full flex-1 flex-col justify-center px-12 almostscreen">
+        <div className='flex flex-col gap-6 text-white widthconstrict'>
+          <h1 className='font-black text-7xl lg:text-9xl'>invader</h1>
+          <h1 className='font-bold text-4xl lg:text-6xl opacity-75'>Full Stack Dev</h1>
+          <div className='mt-8 greenprimary py-8 px-6 md:px-12 rounded-lg max-w-2xl'>
+            <p className='text-black  max-w-lg text-md md:text-lg font-semibold leading-relaxed opacity-85'>Specializing in building and designing fluid user experiences and powerful backend architecture, focused on the web3 space. </p>
+            <div className='flex-col sm:flex-row flex gap-6 mt-8'>
+              <button onClick={saveDiscord} className='bg-black px-8 py-3 rounded-lg font-bold flex flex-row items-center justify-center gap-3'><img className='w-6' src='/discord.svg'/>{disc}</button>
+              <button onClick={saveEmail} className='bg-black px-8 py-3 rounded-lg font-bold flex flex-row items-center justify-center gap-3'><img className='w-6' src='/email.svg'/>{ema}</button>
+            </div>
+          </div>
+         
         </div>
       </main>
+      <div className='min-h-screen greenprimary h-full w-full flex flex-col '>
+        <div className='px-8 h-full divide-y divide-black w-full flex flex-col gap-6 widthconstrict text-black'>
+          <h1 className='font-semibold text-4xl lg:text-6xl mt-16'>Clients</h1>
+          <div className='py-8 flex flex-col gap-8'>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+              <div className='shadow-lg  rounded-lg w-full flex items-center justify-center h-48 flex flex-row agc'>
+                <div className='px-6 flex flex-col gap-3 rounded-l-lg bg-black h-full text-center flex items-center justify-center w-2/4'>
+                  <h1 className='font-bold text-white text-xl sm:text-2xl opacity-75'>Alpha Girl Club</h1>
+                  <h1 className='font-medium text-white text-sm sm:text-lg opacity-75 greentext'>Lead Developer</h1>
+                </div>
+                <div className='rounded-r-lg overflow-hidden bg-red-200 h-full w-full overflow-hidden'>
+                  <img className='object-cover object-center w-full h-full' src='/agc.jpg' />
+                </div>
+              </div>
+
+              <div className='shadow-lg  rounded-lg w-full flex items-center justify-center h-48 flex flex-row agc'>
+                <div className=' px-6 flex flex-col gap-3 rounded-l-lg bg-black h-full text-center flex items-center justify-center w-2/4'>
+                  <h1 className='font-bold text-white text-xl sm:text-2xl opacity-75'>Project Destroyer</h1>
+                  <h1 className='font-medium text-white text-sm sm:text-lg opacity-75 greentext'>Developer</h1>
+                </div>
+                <div className='rounded-r-lg overflow-hidden bg-red-200 h-full w-full overflow-hidden'>
+                  <img className='object-cover object-center w-full h-full' src='/pd.png' />
+                </div>
+            </div>
+
+             <div className='shadow-lg  rounded-lg w-full flex items-center justify-center h-48 flex flex-row agc'>
+                <div className=' px-6 flex flex-col gap-3 rounded-l-lg bg-black h-full text-center flex items-center justify-center w-2/4'>
+                  <h1 className='font-bold text-white text-xl sm:text-2xl opacity-75'>Nightkids</h1>
+                  <h1 className='font-medium text-white text-sm sm:text-lg opacity-75 greentext'>Developer</h1>
+                </div>
+                <div className='rounded-r-lg overflow-hidden bg-red-200 h-full w-full overflow-hidden'>
+                  <img className='object-cover  object-center w-full h-full' src='/nk.jpg' />
+                </div>
+            </div>
+
+            <div className='mb-8 shadow-lg  rounded-lg w-full flex items-center justify-center h-48 flex flex-row agc'>
+                <div className=' px-6 flex flex-col gap-3 rounded-l-lg bg-black h-full text-center flex items-center justify-center w-2/4'>
+                  <h1 className='font-bold text-white text-xl sm:text-2xl opacity-75'>Cat Bricks Clubhouse</h1>
+                  <h1 className='font-medium text-white text-sm sm:text-lg opacity-75 greentext'>Lead Developer</h1>
+                </div>
+                <div className='rounded-r-lg overflow-hidden bg-red-200 h-full w-full overflow-hidden'>
+                  <img className='object-cover  object-center w-full h-full' src='/cbc.png' />
+                </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
